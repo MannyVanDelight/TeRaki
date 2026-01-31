a1a3ccb5631f5b5c7d3c004da92232e1fadca74f
MannyVanDelight <141590410+MannyVanDelight@users.noreply.github.com>
2026-01-31 19:56:57 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -22,8 +22,9 @@
 /* L22 */ // --- 3. CONTROLS ---
 /* L23 */ const controls = new OrbitControls(camera, renderer.domElement);
 /* L24 */ controls.enableDamping = true; 
-/* L25 */ camera.position.set(0, 5, 15); 
-/* L26 */ controls.update();
+/* L25 */ camera.position.set(-11, 10, -7); 
+/* L26 */ controls.target.set(-3, 0, 4);
+/* L26b*/ controls.update();
 /* L27 */ 
 /* L28 */ // --- 4. LOAD MODEL ---
 /* L29 */ const loader = new GLTFLoader();
