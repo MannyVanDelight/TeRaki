9ed63a7e8c6b1a55f1d052318040632806870850
MannyVanDelight <141590410+MannyVanDelight@users.noreply.github.com>
2026-02-01 17:46:18 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -22,8 +22,8 @@
 /* L22 */ // --- 3. CONTROLS ---
 /* L23 */ const controls = new OrbitControls(camera, renderer.domElement);
 /* L24 */ controls.enableDamping = true; 
-/* L25 */ camera.position.set(-7, 10, -6); 
-/* L26 */ controls.target.set(-3, -1, 2);
+/* L25 */ camera.position.set(-5, 10, -7); 
+/* L26 */ controls.target.set(-0.5, -1, 0.8);
 /* L26b*/ controls.update();
 /* L27 */ 
 /* L28 */ // --- 4. LOAD MODEL WITH DRACO ---
