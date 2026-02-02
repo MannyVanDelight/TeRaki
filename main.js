a8ebe6e55fe57ea3977172ef173fc3116fa3c993
MannyVanDelight <141590410+MannyVanDelight@users.noreply.github.com>
2026-02-02 20:47:40 +1300

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ import { PointerLockControls } from 'three/examples/jsm/controls/PointerLockCont
 
 // --- 1. BASIC SETUP ---
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x222222); // Darker background to make the house pop
+scene.background = new THREE.Color(0xff0000); // Darker background to make the house pop
 
 const camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 0.1, 1000);
 // Starting position: slightly back and at a 1.4m "human" height
